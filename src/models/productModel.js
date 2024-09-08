ba73ee80712f9f60fd8a13f37a6a216eeb876325
Brandon Harding <101778503+bharding216@users.noreply.github.com>
2024-09-07 22:35:51 -0500

    Update RDS references

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -3,7 +3,7 @@ const db = require('../config/database');
 class ProductModel {
     getAllProducts() {
         return new Promise((resolve, reject) => {
-            const query = 'SELECT * FROM products';
+            const query = 'SELECT * FROM Products';
             db.query(query, (error, results, fields) => {
                 if (error) {
                     reject(error);
@@ -16,7 +16,7 @@ class ProductModel {
 
     getProductById(productId) {
         return new Promise((resolve, reject) => {
-            const query = 'SELECT * FROM products WHERE id = ?';
+            const query = 'SELECT * FROM Products WHERE id = ?';
             db.query(query, [productId], (error, results, fields) => {
                 if (error) {
                     reject(error);
